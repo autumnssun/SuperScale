b47225cc77e3b35d4d359c1e0556b522ff0fd244
Khoa Tran <autumns.sun@gmail.com>
2015-11-13 22:00:23 +1000

    added arduino libraries

--- ArduinoCode/SuperScale/SuperScale.ino
+++ ArduinoCode/SuperScale/SuperScale.ino
@@ -1,24 +1,17 @@
 #include <HX711.h>
 
 //HX711 hx(9, 10, 128, 0.00127551);
-HX711 hx(9, 10, 128, -0.0025);
+HX711 hx(8, 7, 128, -0.0025);
 //-0.1966101695
 
-
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   Serial.begin(9600);
-  hx.tare(10);
-
-  pinMode(ledPin, OUTPUT);      
-  pinMode(buttonPin, INPUT);     
+  hx.tare(10); 
 
 }
 void loop() {
-  checkbtn();
   
   double sum0 = 0;
   double sum1 = 0;
@@ -30,23 +23,3 @@ void loop() {
   Serial.println(sum1/10);
   //delay(10);
 }
-
-
-void checkbtn(){
- buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {     
-    // turn LED on:    
-    digitalWrite(ledPin, HIGH);
-    hx.tare(10);
-    delay();
-  } 
-  else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW); 
-  }
-}
-
-
